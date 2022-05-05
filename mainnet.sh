ec3d416185d7610bc3a20f2342a56ad6c11feb66
Denver <aeharvlee@gmail.com>
2022-05-05 10:32:07 +0800

    Update mainnet.sh
    Co-authored-by: Jamie <32922423+jimni1222@users.noreply.github.com>

--- mainnet.sh
+++ mainnet.sh
@@ -4,6 +4,6 @@
 export MODE=ONLINE
 export NETWORK=MAINNET
 export PORT=8080
-export KEN="<Replace this with Acrhive Node URL>
+export KEN="<Replace this with Archive Node URL>"
 
 go run main.go run >> ros-mainnet.log
