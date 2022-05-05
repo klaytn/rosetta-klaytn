1b20296978119300f458eeef1dc6df08d8c1e0c0
Denver <aeharvlee@gmail.com>
2022-05-05 10:25:28 +0800

    Applied recommended configuration from coinbase and add running scripts
    Also updated some configurations

---
+++ mainnet.sh
@@ -0,0 +1,9 @@
+# Run rosetta-klaytn for mainnet
+# Use this script when you want to run it in the terminal.
+
+export MODE=ONLINE
+export NETWORK=MAINNET
+export PORT=8080
+export KEN="<Replace this with Acrhive Node URL>
+
+go run main.go run >> ros-mainnet.log
