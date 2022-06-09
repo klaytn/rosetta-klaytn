3d652692548b12cf64794445c7898cffa577ad71
Jamie <wlalsdlwwkd@gmail.com>
2022-06-09 13:35:59 +0900

    Updated version to 1.0.0

--- services/network_service_test.go
+++ services/network_service_test.go
@@ -32,7 +32,7 @@ import (
 
 var (
 	// rosetta-klaytn version
-	middlewareVersion = "0.0.1"
+	middlewareVersion = "1.0.0"
 
 	defaultNetworkOptions = &types.NetworkOptionsResponse{
 		Version: &types.Version{
